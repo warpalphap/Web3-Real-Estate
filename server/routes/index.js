5087f59336971099947d58297da1a0cfb5b44d7e
sunshine19817 <phantom19817@gmail.com>
2024-08-20 05:46:42 -0300

    demo version

---
+++ server/routes/index.js
@@ -0,0 +1,11 @@
+const orderRouter = require('./orderRoute')
+const paymentRouter = require('./paymentRoute')
+const projectRouter = require('./productRoute')
+const userRouter = require('./userRoute')
+
+module.exports = {
+    orderRouter,
+    paymentRouter,
+    projectRouter,
+    userRouter
+}
